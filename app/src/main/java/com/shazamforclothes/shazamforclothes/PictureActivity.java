1fe0eba9eaa591b1ce4332d0d1b306a108ac8f3f
pjohn1203 <33132277+pjohn1203@users.noreply.github.com>
2017-11-10 23:52:51 -0800

    Photo Functionality

--- app/src/main/java/com/shazamforclothes/shazamforclothes/PictureActivity.java
+++ app/src/main/java/com/shazamforclothes/shazamforclothes/PictureActivity.java
@@ -7,18 +7,72 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.ImageView;
 import java.io.File;
+import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
 
-public class PictureActivity extends AppCompatActivity {
+
+
+public class PictureActivity extends Activity {
 
     private File imageFile;
+    Button buttonCapture;
+    ImageView DisplayImage;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+
+        Button buttonCapture = (Button)findViewById(R.id.button3);
+        ImageView DisplayImage = (ImageView)findViewById(R.id.PictureActivityView);
+
+        buttonCapture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                /*
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+                */
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager())!=null){
+                    startActivityForResult(takePictureIntent,CAM_REQUEST);
+                }
+
+            }
+        });
+
     }
+
+    private File getFile(){
+
+        File folder = new File("sdcard/camera_app");
+
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+        File image_file = new File(folder, "cam_image");
+        return image_file;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAM_REQUEST && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            DisplayImage.setImageBitmap(imageBitmap);
+        }
+    }
+
+    /*
 
     public void takepicture(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -28,4 +82,8 @@ public class PictureActivity extends AppCompatActivity {
         intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY , 1);
         startActivityForResult(intent, 0);
     }
+
+    */
+
+
 }
